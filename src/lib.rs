c295953d124fc06713051803096f5f0d6ae65458
Rokonio <naelkanada@gmail.com>
2021-07-31 19:49:27 +0200

    Documenting and improving readability

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,31 @@
-//! Utilities for generating word
-//!
-//! Generate word that souds like word from a language.
-//! The language must be a [`BufReader`] that has one word per line
-//!
-//! # Usage
-//!
+// By Rokonio <naelkanada+rust@gmail.com>
+// This crate is under the MIT liscence <LISCENCE or
+// https://opensource.org/licenses/MIT>
 
+//! Utilities for generating random words based on a language
+//!
+//! Word_generator provides utilities for generating words based on a language.
+//! It create a ProbabilityTable (see
+//! [Markov Chains](https://en.wikipedia.org/wiki/Markov_chain#Examples)) by
+//! analyzing a language that can then generate words that souds like this
+//! language.
+//!
+//! # Example
+//!
+//! ```
+//! # fn main() -> std::io::Result<()> {
+//! use std::{fs::File, io::BufReader};
+//! use word_generator::*;
+//!
+//! let reader = BufReader::new(File::open("Fr.txt")?);
+//! let table = ProbabilityTable::from_reader(reader, 3)?;
+//!
+//! println!("{:?}", table.generate_words(15)); // Generate 15 word
+//! // Is the same as this
+//! // println!("{:?}", generate_words(reader, 3, 15));
+//! # Ok(())
+//! # }
+//! ```
 use std::{
     collections::HashMap,
     io::{self, prelude::*},
@@ -14,76 +34,114 @@ use std::{
 use rand::distributions::WeightedIndex;
 use rand::prelude::*;
 
-// The matrix of chance (u32) of a lettre (char) to appear
-// after a sequence of lettres (String) of size state_size
+/// This type represents represents the probability table of each caracters for
+/// a language to appears after a given string of a certain length.
+///
+/// It is used as a generator to create words that souds like the language that
+/// was analyzed.
+///
+/// # Example
+///
+/// ```
+/// # fn main() -> std::io::Result<()> {
+/// use std::{fs::File, io::BufReader};
+/// use word_generator::*;
+///
+/// let reader = BufReader::new(File::open("Fr.txt")?);
+/// let table = ProbabilityTable::from_reader(reader, 3)?;
+///
+/// println!("{:?}", table.generate_words(15)); // Generate 15 word
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Debug)]
-pub struct GeneratedMatrix {
-    matrix: HashMap<String, HashMap<char, u32>>,
-    state_size: usize,
+pub struct ProbabilityTable {
+    table: HashMap<String, HashMap<char, u32>>,
+    input_length: usize,
 }
 
-impl GeneratedMatrix {
-    fn new(state_size: usize) -> GeneratedMatrix {
-        GeneratedMatrix {
-            matrix: HashMap::new(),
-            state_size,
+impl ProbabilityTable {
+    fn new(input_length: usize) -> ProbabilityTable {
+        ProbabilityTable {
+            table: HashMap::new(),
+            input_length,
         }
     }
 
-    pub fn from_reader(reader: impl BufRead, state_size: usize) -> io::Result<GeneratedMatrix> {
-        Ok(generate_matrix(add_space(reader, state_size)?, state_size))
+    /// Construct a new `ProbabilityTable` from reader. It can be any type that
+    /// implements [`BufRead`].
+    pub fn from_reader(reader: impl BufRead, input_length: usize) -> io::Result<ProbabilityTable> {
+        Ok(generate_table(
+            add_space(reader, input_length)?,
+            input_length,
+        ))
     }
 
-    pub fn generate_words(&self, number: u32) -> Vec<String> {
-        generate_multiple_words(self, number)
+    /// Generate `amount` words.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # fn main() -> std::io::Result<()> {
+    /// use std::{fs::File, io::BufReader};
+    /// use word_generator::*;
+    ///
+    /// let reader = BufReader::new(File::open("Fr.txt")?);
+    /// let table = ProbabilityTable::from_reader(reader, 3)?;
+    ///
+    /// println!("{:?}", table.generate_words(15)); // Generate 15 word
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn generate_words(&self, amount: u32) -> Vec<String> {
+        generate_multiple_words(self, amount)
     }
 }
 
-// Add space of size state_size beetween each word (1 word per line)
-fn add_space(reader: impl BufRead, state_size: usize) -> io::Result<String> {
+// Replace each new line characters by a series of space of length
+fn add_space(reader: impl BufRead, length: usize) -> io::Result<String> {
     reader
         .lines()
-        .map(|line| -> io::Result<String> { Ok(format!("{}{}", " ".repeat(state_size), line?)) })
+        .map(|line| -> io::Result<String> {
+            Ok(format!("{}{}", " ".repeat(length), line?.to_lowercase()))
+        })
         .collect::<io::Result<String>>()
 }
 
-// Generate the matrix from the output of add_space
-fn generate_matrix(spaced_file: String, state_size: usize) -> GeneratedMatrix {
-    let mut matrix = GeneratedMatrix::new(state_size);
+// Generate a ProbabilityTable from the output of add_space
+fn generate_table(spaced_file: String, input_length: usize) -> ProbabilityTable {
+    let mut table = ProbabilityTable::new(input_length);
     let chars_list: Vec<_> = spaced_file.chars().collect();
-    for charactere in 0..chars_list.len() - state_size {
+    for charactere in 0..chars_list.len() - input_length {
         let key: String = chars_list
-            .get(charactere..charactere + state_size)
+            .get(charactere..charactere + input_length)
             .unwrap()
             .iter()
             .collect();
-        let value: char = *chars_list.get(charactere + state_size).unwrap();
 
-        let counter = matrix.matrix.entry(key).or_default();
-        let ccounter = counter.entry(value).or_insert(0);
-        *ccounter += 1;
+        let value: char = *chars_list.get(charactere + input_length).unwrap();
+
+        *table
+            .table
+            .entry(key)
+            .or_default()
+            .entry(value)
+            .or_default() += 1;
     }
-    matrix
+    table
 }
 
-// Generate one word from the matrix
-fn generate_word(matrix: &GeneratedMatrix) -> String {
-    let mut out = " ".repeat(matrix.state_size);
-    let mut rng = thread_rng();
+// Generate one word from a ProbabilityTable
+fn generate_word(table: &ProbabilityTable, rng: &mut ThreadRng) -> String {
+    let mut out = " ".repeat(table.input_length);
     loop {
         let chars_list: Vec<_> = out.chars().collect();
-        let choices = matrix
-            .matrix
-            .get(
-                &chars_list[chars_list.len() - matrix.state_size..]
-                    .iter()
-                    .collect::<String>(),
-            )
-            .unwrap();
-        let next_letter =
-            choices.keys().collect::<Vec<&char>>()[WeightedIndex::new(choices.values())
-                .unwrap()
-                .sample(&mut rng)];
+        let key = &chars_list[chars_list.len() - table.input_length..]
+            .iter()
+            .collect::<String>();
+        let choices = table.table.get(key).unwrap();
+        let weight = WeightedIndex::new(choices.values()).unwrap();
+        let next_letter = choices.keys().collect::<Vec<&char>>()[weight.sample(rng)];
         out += &next_letter.to_string();
         if out.ends_with(' ') {
             break;
@@ -92,28 +150,31 @@ fn generate_word(matrix: &GeneratedMatrix) -> String {
     out.trim().to_string()
 }
 
-fn generate_multiple_words(matrix: &GeneratedMatrix, number: u32) -> Vec<String> {
+fn generate_multiple_words(matrix: &ProbabilityTable, number: u32) -> Vec<String> {
     let mut vec_string = Vec::new();
+    let mut rng = thread_rng();
     for _ in 0..number {
-        vec_string.push(generate_word(&matrix))
+        vec_string.push(generate_word(&matrix, &mut rng));
     }
     vec_string
 }
 
 /// Genrate words that souds like those from a language by analyzing it
 /// (from a reader containing each word on a different line), with the
-/// precision of `state_size` (how many letters are take in consideration when
+/// precision of `input_length` (how many letters are take in consideration when
 /// generating the next letter) and the number of word to generate
+
+/// Generate `amount` word with choosable `accuracy` by analyzing a language
+/// under the form of a type that implement [`BufRead`]
 pub fn generate_words(
     reader: impl BufRead,
-    state_size: usize,
-    number_of_words: u32,
+    accuracy: usize,
+    amout: u32,
 ) -> io::Result<Vec<String>> {
     let mut out = generate_multiple_words(
-        &generate_matrix(add_space(reader, state_size)?, state_size),
-        number_of_words,
+        &generate_table(add_space(reader, accuracy)?, accuracy),
+        amout,
     );
     out.sort_by_key(|a| a.len());
-    // out.sort_by_key();
     Ok(out)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,8 @@ use std::{
 
 fn main() -> io::Result<()> {
     let reader = BufReader::new(File::open("Fr.txt")?);
-    let state_size = 3;
+    let accuracy = 3;
     let number_of_words = 15;
-    println!(
-        "{:#?}",
-        generate_words(reader, state_size, number_of_words)?
-    );
+    println!("{:#?}", generate_words(reader, accuracy, number_of_words)?);
     Ok(())
 }
